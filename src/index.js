9af608f3e863b5b81578c61347cab730569c46d9
ku-cat <kairintyo@ku.local>
2020-01-11 19:12:48 +0900

    notes_compo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+axios.get("http://localhost:3001/notes").then(response => {
+  const notes = response.data;
+  ReactDOM.render(<App notes={notes} />, document.getElementById("root"));
+});
